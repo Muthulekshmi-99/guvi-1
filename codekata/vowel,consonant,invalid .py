52f851cd67bdae6ae01dfa36b2da9c03db7dad2d
Muthu206balu <51689768+Muthu206balu@users.noreply.github.com>
2019-06-12 12:03:38 +0530

    Update vowel,consonant,invalid .py

--- codekata/vowel,consonant,invalid .py
+++ codekata/vowel,consonant,invalid .py
@@ -3,6 +3,6 @@ if(vow.isalpha()):
   if (vow=="a" or vow=="e" or vow=="i" or vow=="o" or vow=="u" or vow=="A" or vow=="E" or vow=="I" or vow=="O" or vow=="u"):
     print("vowel")
   else:
-    print("consonant")
+    print("Consonant")
 else:
     print("invalid")
