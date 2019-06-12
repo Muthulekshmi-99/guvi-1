393e34c013df8697741400f7d88d78c6c59020ce
Muthu206balu <51689768+Muthu206balu@users.noreply.github.com>
2019-06-12 12:00:22 +0530

    Create vowel,consonant,invalid .py

---
+++ codekata/vowel,consonant,invalid .py
@@ -0,0 +1,8 @@
+vow=input()
+if(vow.isalpha()):
+  if (vow=="a" or vow=="e" or vow=="i" or vow=="o" or vow=="u" or vow=="A" or vow=="E" or vow=="I" or vow=="O" or vow=="u"):
+    print("vowel")
+  else:
+    print("consonant")
+else:
+    print("invalid")
